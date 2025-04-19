745655b17505bbaafb62e2be73a4b1b93b92a569
adavorn <jacob46king@gmail.com>
2025-04-19 12:38:33 -0500

    added style to the home.js

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -5,10 +5,11 @@ function Home() {
   return (
     <div className="home-container">
       <div className="home-card">
-        <h1>Baseball FAQ</h1>
-        <div className="home-links">
-          <Link to="/login" className="home-link">Login</Link>
-          <Link to="/register" className="home-link">Register</Link>
+        <h1>Welcome to the Baseball FAQ</h1>
+        <p className="home-subtext">Learn something new.</p>
+        <div className="home-buttons">
+          <Link to="/login" className="home-button">Login</Link>
+          <Link to="/register" className="home-button outline">Register</Link>
         </div>
       </div>
     </div>
